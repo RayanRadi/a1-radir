c2991216a015d18c1236dc6374e4a00edf18efe2
RayanRadi <radirayan005@gmail.com>
2025-01-22 12:07:19 -0500

    Feature: Parse and load a simple maze

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Maze.java
@@ -1,42 +1,18 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
-/**
- * Represents the maze structure.
- */
 public class Maze {
-    private char[][] grid; // 2D representation of the maze (walls and passages)
+    private char[][] grid;
 
-    /**
-     * Constructor for the Maze class
-     * TO DO: Implement logic to initialize the grid from a file or input.
-     */
-    public Maze(char[][] grid) {
-        this.grid = grid;
+    public Maze() {
+        // Hardcoded straight-line maze for MVP
+        this.grid = new char[][] {
+            {'#', '#', '#', '#', '#'},
+            {'#', ' ', ' ', ' ', '#'},
+            {'#', '#', '#', '#', '#'}
+        };
     }
 
-    /**
-     * width of the maze .
-     */
-    public int getWidth() {
-        // TODO: Return the width of the grid.
-        return grid[0].length;
-    }
-
-    /**
-     *  height of the maze 
-     */
-    public int getHeight() {
-        // TODO: return the height 
-        return grid.length;
-    }
-
-    /**
-     * Gets the cell at a given position.
-     * param x Column index.
-     * param y Row index.
-     * return The character at the specified position.
-     */
-    public char getCell(int x, int y) {
-        return grid[y][x];
+    public char[][] getGrid() {
+        return grid;
     }
 }
